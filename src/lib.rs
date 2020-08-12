b8d012f5144fe8c3ee3579d1e605dae8bb7a74a8
kwantam <kwantam@gmail.com>
2020-08-11 22:15:26 -0700

    bump version to 0.0.4

--- src/lib.rs
+++ src/lib.rs
@@ -126,5 +126,5 @@ mod consts {
     pub(crate) const DFLT_PORT: u16 = 1719;
     pub(crate) const ALPN_CONEC: &[&[u8]] = &[b"conec"];
     pub(crate) const MAX_LOOPS: usize = 8;
-    pub(crate) const VERSION: &str = "CONEC_V0.0.3::";
+    pub(crate) const VERSION: &str = "CONEC_V0.0.4::";
 }
